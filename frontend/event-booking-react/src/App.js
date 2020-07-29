2ca5f7a511052a6857c9378da3929ec0d134c785
vanessaMora702 <vanessamora702@gmail.com>
2020-07-29 19:28:37 -0400

    Created login form and styled it. Allow user to login or signup using graphQL api, added Cors headerr to the backend

--- frontend/event-booking-react/src/App.js
+++ frontend/event-booking-react/src/App.js
@@ -4,17 +4,23 @@ import './App.css';
 import AuthPage from './pages/Auth';
 import EventPage from './pages/Event';
 import BookingPage from './pages/Booking';
+import MainNavigation from './components/Navigation/MainNavigation';
 
 
 function App() {
   return (
       <BrowserRouter>
+      <React.Fragment>
+      <MainNavigation/>
+        <main>
         <Switch>
         <Redirect from="/" to="/auth" exact/>
         <Route path="/auth" component={AuthPage}/>
         <Route path="/events" component={EventPage}/>
         <Route path="/bookings" component={BookingPage}/>
         </Switch>
+        </main>
+        </React.Fragment>
       </BrowserRouter>
   );
 }
--- frontend/event-booking-react/src/components/Navigation/MainNavigation.js
+++ frontend/event-booking-react/src/components/Navigation/MainNavigation.js
@@ -6,7 +6,7 @@ import './MainNavigation.css';
 const mainNavigation = props => (
   <header className="main-navigation">
     <div className="main-navigation__logo">
-      <h1>EasyEvent</h1>
+      <h1>Event Booking</h1>
     </div>
     <nav className="main-navigation__items">
       <ul>
